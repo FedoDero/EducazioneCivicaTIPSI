0da52196333523d2c7884b8e1d05ad7eafe1dde8
FedoDero <odranoel.izzodef@gmail.com>
2023-04-02 22:37:30 +0200

    primo commit

---
+++ progetto tecnologie FedoDero/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progetto_tecnologie_FedoDero
+{
+    internal class Class1
+    {
+        string alpha = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+        public Class1()
+        {
+
+        }
+        
+        public void cripto1(string mess, int n)
+        {
+            foreach (var i in mess) {
+                foreach (var j in alpha) {
+                    if (mess[i] == alpha[j])
+                    {
+                        mess.Replace(mess[i], alpha[j+n]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ progetto tecnologie FedoDero/Program.cs
@@ -0,0 +1,7 @@
+﻿using progetto_tecnologie_FedoDero;
+
+Console.WriteLine("Hello, World!");
+
+Class1 class1 = new Class1();
+
+class1.cripto1("ciao", 5);
